6b869651439970853b19d337efd6d452ed830c99
Jane Iaskevich <73097670+jennyfox@users.noreply.github.com>
2020-11-16 20:48:58 +0300

    git i

---
+++ Assets/Scripts/script.js
@@ -0,0 +1,81 @@
+// use strict
+// let age = prompt ("How old are you?", 
+// '')
+//     if (age >= 14 && age <= 90) {
+//     alert ("Your age is acceptable")
+// }   else {
+//     alert ("Sorry, you can't get access")
+// }
+
+// let age = prompt ("How old are you?", 
+// '')
+//     if (!(age >= 14 && age <= 90)) {
+//     alert ("Your age is acceptable")
+// }   else {
+//     alert ("Sorry, you can't get access")
+// }
+
+// let login = prompt ("Введите логин", '');
+//     if (login = "Админ") {
+//         let pass = prompt ("Введите пароль", '');
+//             if (pass = "Я главный") {
+//                 alert ("Здравствуйте")
+//             } 
+//             else {
+//                 if (pass = '' || pass = null) {
+//                 alert ("Отменено")
+//                 } 
+//                 else {
+//                 alert ("Неверный пароль")
+//                 }
+//             }       
+//     else {
+//         if (login = '' || login = null) {
+//             alert ("Отменено")
+//         } 
+//        else {
+//             alert ("Я вас не знаю")
+//        }
+//     }
+
+// Task 1
+
+let n = 'number';
+let s = 'symbol';
+let str = 'string';
+let b = 'boolean';
+let bi = 'bigint';
+let o = 'object';
+let nu = 'null';
+let u = 'undefined';
+alert (s);
+alert (str);
+alert (b);
+alert (bi);
+alert (o);
+alert (nu);
+alert (u);
+console.log (n, s, str, b, bi, o, nu, u);
+
+// Task 2
+
+// let age = prompt ('Сколько вам лет?');
+// alert ('Поздравляем, вам ' + age + ' полных лет!');
+// if (age >= 18) {
+//   alert ('Совершеннолетний пользователь');
+// } else {
+//   alert ('Несовершеннолетний пользователь');
+// }
+
+// // Task 3
+
+// let numbers = [10, 25, 100];
+// let a = numbers [0];
+// let b = numbers [1];
+// let c = numbers [2];
+// alert ('Давайте сложим числа ' + a + ', ' + b + ', ' + c);
+// function sum (a, b, c) {
+//   return a + b + c;
+// }
+// let result = sum (a, b, c);
+// alert ('Сумма этих чисел равна ' + result);
