16ef1807080334829b8e3f9cf932063cf261f345
Jane Iaskevich <73097670+jennyfox@users.noreply.github.com>
2020-11-17 18:39:49 +0300

    js 4

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -79,3 +79,66 @@ console.log (n, s, str, b, bi, o, nu, u);
 // }
 // let result = sum (a, b, c);
 // alert ('Сумма этих чисел равна ' + result);
+
+Task 4 
+
+function hello1 () {
+    return ('Привет, JavaScript!');
+}
+hello1 ();
+
+Task 5 
+
+function hello2 (name = 'гость') {
+    alert ('Привет' + ', ' + name);
+
+}
+hello2 ('Василий');
+hello2 ();
+
+Task 6
+
+function mul (n,m) {
+    return n * m;
+}
+mul (3, 6);
+
+Task 7
+
+function repeat (str = '', n = 2) {
+    return (str * 2);
+}
+repeat ('Повтори меня');
+
+Task 8
+
+function rgb(a = 0, b = 0, c = 0) {
+    return (a + ', ' + b + ', ' + c);
+}
+rgb (23, 100, 134);
+
+Task 9
+
+function avg (a, b, c) {
+    return ((a + b + c) / 3);
+}
+avg (4, 9, 15);
+
+Task 10 
+    (Я ограничила число до 25, я не знаю, как выразить формулой 
+    "все числа, заканчивающиеся на 2, 3 и 4" и пр.)
+
+function words (n = 0) {
+    if (n = 1 || n = 21) {
+        alert (n + ' товар');
+    }
+    if (n >= 2 && n <= 4 || n >= 22 && n <= 24) {
+        alert (n + ' товара');
+    }
+    if (n >= 5 && n <= 20 || n = 25) {
+        alert (n + ' товаров');
+    }
+}
+alert (words (1));
+alert (words (5));
+alert (words (23));
