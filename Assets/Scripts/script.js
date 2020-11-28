a08010394f4a1bdc8241bb3aaaf9111ab39e4903
Jane Iaskevich <73097670+jennyfox@users.noreply.github.com>
2020-11-29 00:35:39 +0300

    29-11

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -265,8 +265,8 @@ alert (result);
 //   						  {id: 2, name: "Петя", age: 19},
 //   						  {id: 3, name: "Маша", age: 18}
 // 						];
-// 	  Добавить в этот маccив еще одного пользователя {id: 4, name: "Дима", age: 17}. Инвертируйте данный массив и выведите возраст пользователя с именем "Петя".
-	   
+// 	  Добавить в этот маccив еще одного пользователя {id: 4, name: "Дима", age: 17}. 
+// Инвертируйте данный массив и выведите возраст пользователя с именем "Петя".
 //     - В массиве users посчитайте общий суммарный возраст всех пользователей.
     
 let users = [
@@ -282,3 +282,113 @@ alert (user.age);
 let result = users.reduce ((sum, item) => sum + item.age, 0);
 alert (result);
 
+// Дан массив var arr = [1, 2, 3, 4, 4, 5, 2, 9, 4]; На основании этого массива 
+// создать новый массив arrUnique который будет представлять собой массив arr 
+// но без дублирующихся по своим значениям элементам. Использовать методы reduce и 
+// indexOf.
+
+
+let arr = [1, 2, 3, 4, 4, 5, 2, 9, 4];
+let arrUnique = arr.reduce((newArr, item) => {
+        console.log(newArr, newArr.indexOf(item));
+        if (newArr.indexOf(item) === -1) {
+            newArr.push(item);
+            return newArr;
+        } else {
+            return newArr;
+        }
+    }, [])
+
+// Дан массив пользователей. var users = [{id: 1, name: "Вася", age: 19}, 
+// {id: 2, name: "Петя", age: 18}, {id: 3, name: "Маша", age: 17}, 
+// {id: 4, name: "Света", age: 18}, {id: 5, name: "Наташа", age: 17}, 
+// {id: 6, name: "Женя", age: 12}, {id: 7, name: "Коля", age: 30},
+// {id: 8, name: "Настя", age: 16}, {id: 9, name: "Антон", age: 19},
+//  {id: 10, name: "Иван", age: 20}, {id: 11, name: "Павел", age: 16},
+// {id: 12, name: "Денис", age: 20}];
+// На основании этого массива сформировать новый массив usersAfterYear где все 
+// пользователи повзрсолели на год и являются совершенолетними (повзрослевших на год, 
+// но оставшимися все еще несовершенолетних пользователей в новый массив 
+// usersAfterYear не включать). Сам массив users должен остаться неизменным. 
+// Использовать метод map.
+
+let users = [
+    {id: 1, name: "Вася", age: 19},
+    {id: 2, name: "Петя", age: 18},
+    {id: 3, name: "Маша", age: 17},
+    {id: 4, name: "Света", age: 18},
+    {id: 5, name: "Наташа", age: 17},
+    {id: 6, name: "Женя", age: 12},
+    {id: 7, name: "Коля", age: 30},
+    {id: 8, name: "Настя", age: 16},
+    {id: 9, name: "Антон", age: 19},
+    {id: 10, name: "Иван", age: 20},
+    {id: 11, name: "Павел", age: 16},
+    {id: 12, name: "Денис", age: 20}
+];
+
+let one = users.map(item => item.age + 1);
+console.log(one);
+let usersAfterYear = [];
+for(let i=0; i < one.length && one.age > 17; i++) {
+    usersAfterYear.push(one[i]);
+};
+console.log(usersAfterYear);
+// не работает
+
+let usersAfterYear = users.map(item => item.age + 1); 
+console.log(usersAfterYear);
+usersAfterYear.filter(item => item.age > 17);
+console.log(usersAfterYear);
+// не работает
+
+
+//  На основании массива usersAfterYear, полученного на основании предыдущего задания 
+//  создать новый массив usersAfterYearUniqueAge, в котором нет пользователей с 
+//  одинаковым возрастом. Если у вас есть два пользователя ({id: 2, name: "Петя", age: 19} 
+//  и {id: 4, name: "Света", age: 19}, то вы должны оставить только Петю или Свету 
+//  одного а не их двоих). (Это задание похоже на первое задания про массив arr и 
+// arrUnique)
+
+let usersAfterYearUniqueAge = usersAfterYear.reduce((a, item) => {
+    console.log(a, a.indexOf(item.age));
+    if (a.indexOf(item.age) === -1) {
+        a.push(item);
+        return a;
+    } else {
+        return a;
+    }
+}, [])
+
+// Дан объект var users = {
+// 	first_name: "pavel",
+// 	second_name: "ivanov",
+// 	father_name: "ivanovich",
+// 	city: "minsk",
+// 	car: "subaru",
+// 	telephone: "samsung" 
+// }
+// Необходимо сделать так чтобы все значения полей объекта users начинались с 
+// заглавной буквы (например не "pavel" a "Pavel"). Для решения этой задачи 
+// рекомендую использовать методы Object.keys или Object.values и методы 
+// строк как UpperCase. 
+
+let users = {
+	first_name: "pavel",
+	second_name: "ivanov",
+	father_name: "ivanovich",
+	city: "minsk",
+	car: "subaru",
+	telephone: "samsung" 
+}
+let keys = Object.keys(users);
+keys.forEach(key => {
+    users[key] = users[key][0].toUpperCase() + users[key].slice(1);
+})
+
+// Посчитать первые 8 чисел Фибоначчи использую рекурсию. Например: fibonachi(n); 
+// (При вызове функции положите n = 8); 
+
+
+// Посчитать с помощью рекурсии факториал числа 10. Например: factorial(n); 
+// (При выозове полжите n = 10); 	
